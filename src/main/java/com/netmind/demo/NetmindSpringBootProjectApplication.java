92cd835aa7bfd399f09d240edc6d9b7d51035c6d
petrusCode <pedro_hot32@hotmail.com>
2021-05-05 20:19:44 +0200

    modularidad improvements with Security config

--- src/main/java/com/netmind/demo/NetmindSpringBootProjectApplication.java
+++ src/main/java/com/netmind/demo/NetmindSpringBootProjectApplication.java
@@ -5,15 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.netmind.demo.service.JWTAuthorizationFilter;
 
 @SpringBootApplication
+@Configuration
 public class NetmindSpringBootProjectApplication {
 
 	static final Logger logger = LoggerFactory
@@ -24,31 +18,6 @@ public class NetmindSpringBootProjectApplication {
 		SpringApplication.run(NetmindSpringBootProjectApplication.class, args);
 		logger.info("My message after start application");
 		System.out.println("Hola Mundo");
-
-	}
-
-	@EnableWebSecurity
-	@Configuration
-	public static class ApiWebSecurityConfigurationAdapter
-			extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-
-			http.csrf().disable()
-					.addFilterAfter(new JWTAuthorizationFilter(),
-							UsernamePasswordAuthenticationFilter.class)
-					.authorizeRequests()
-					.antMatchers(HttpMethod.GET, "/", "/v2/api-docs", // swagger
-							"/webjars/**", // swagger-ui webjars
-							"/swagger-resources/**", // swagger-ui resources
-							"/configuration/**", // swagger configuration
-							"/*.html", "/favicon.ico", "/**/*.html",
-							"/**/*.css", "/**/*.js")
-					.permitAll().antMatchers(HttpMethod.POST, "/user")
-					.permitAll().anyRequest().authenticated();
-
-		}
 
 	}
 
